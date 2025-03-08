bdf568826bce4063d5b91c8ac5372ac64bc104cc
Satyasuranjeet <satyajena911@gmail.com>
2025-03-09 00:26:46 +0530

    Added a major change

--- api/index.py
+++ api/index.py
@@ -2,6 +2,12 @@ from app import app
 
 def handler(event, context):
     from werkzeug.wrappers import Request, Response
-    request = Request(event)
-    response = Response()
-    return app(request.environ, response.start_response)
+    try:
+        request = Request(event)
+        response = Response()
+        return app(request.environ, response.start_response)
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": f"Internal server error: {str(e)}"
+        }
